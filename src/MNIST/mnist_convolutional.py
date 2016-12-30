c7eb37461fc3b3c9cdcfcc75fedcc71b734c1066
ozanerhansha <weegeesixtyfour@gmail.com>
2016-12-29 20:15:16 -0500

    Added Tensorboard Graph
    Added Graph of Convolutional NN nodes via Tensorboard

--- src/MNIST/mnist_convolutional.py
+++ src/MNIST/mnist_convolutional.py
@@ -17,7 +17,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 #Save path for network tensors.
-save_path = 'C:\\Users\Weegee 64\Source Code\workspace\\NeuralNetworks\src\MNIST\mnist_convolutional_saved\conv_mnist.chk'
+save_path = 'C:\\Users\Weegee 64\Source Code\workspace\\NeuralNetworks\src\MNIST\mnist_convolutional_saved'
 
 #Filter Constants
 filter_size = 5 #5x5 filter
@@ -61,75 +61,84 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, ksize=[1, pooling_size, pooling_size, 1], strides=[1, pooling_size, pooling_size, 1], padding='SAME')
 
 #General convolutional layer with pooling
-def conv_layer(x,inputChannels,outputChannels,filterSize):
-    W = weight_variable([filterSize, filterSize, inputChannels, outputChannels])
-    b = bias_variable([outputChannels])
-    y = tf.nn.relu(conv2d(x, W) + b) #Applies filters then adds bia
-    y_pool = max_pool_2x2(y) #Halves resolution
+def conv_layer(x,inputChannels,outputChannels,filterSize,name):
+    with tf.name_scope(name):
+        W = weight_variable([filterSize, filterSize, inputChannels, outputChannels])
+        b = bias_variable([outputChannels])
+        y = tf.nn.relu(conv2d(x, W) + b) #Applies filters then adds bia
+        y_pool = max_pool_2x2(y) #Halves resolution
     return y_pool
 
 #General fully connected layer
-def fullycon_layer(x,inputChannels,outputChannels):
-    W = weight_variable([inputChannels, outputChannels])
-    b = bias_variable([outputChannels])
-    y = tf.matmul(x, W) + b
+def fullycon_layer(x,inputChannels,outputChannels,name):
+    with tf.name_scope(name):
+        W = weight_variable([inputChannels, outputChannels])
+        b = bias_variable([outputChannels])
+        y = tf.matmul(x, W) + b
     return y
 
 #Input Layer
-x = tf.placeholder(tf.float32, shape=[None, img_size_flat]) #nx784 Matrix (Input)
-x_image = tf.reshape(x, [-1,img_size,img_size,1]) #Reshape x to 28x28x1 Tensor (1 color channel)
+with tf.name_scope('input'):
+    x = tf.placeholder(tf.float32, shape=[None, img_size_flat],name='x-input') #nx784 Matrix (Input)
+    x_image = tf.reshape(x, [-1,img_size,img_size,1],name='x-input-img') #Reshape x to 28x28x1 Tensor (1 color channel)
 
 #Convolutional Layer 1
-conv1 = conv_layer(x_image, channels[0], channels[1], filter_size)
+conv1 = conv_layer(x_image, channels[0], channels[1], filter_size,name='Conv._Layer_1')
 
 #Convolutional Layer 2
-conv2 = conv_layer(conv1, channels[1], channels[2], filter_size)
-
-#Conv. Layer 2 (Flattened for FC Layer 1)
-'''Flattens the output of conv layer 2 to fit the linear fc layer 1'''
-conv2_flat = tf.reshape(conv2, [-1, channels[3]])
+with tf.name_scope('Conv.Layer_2'):
+    conv2 = conv_layer(conv1, channels[1], channels[2], filter_size,name='Convolution')
+    '''Flattens the output of conv layer 2 to fit the linear fc layer 1'''
+    conv2_flat = tf.reshape(conv2, [-1, channels[3]],name='Flatten_Conv_2') #Conv. Layer 2 (Flattened for FC Layer 1)
 
 #Fully Connected Layer 1
-fc1 = fullycon_layer(conv2_flat, channels[3], channels[4])
+with tf.name_scope('FC_Layer_1'):
+    fc1 = fullycon_layer(conv2_flat, channels[3], channels[4],name='FC_Layer_1')
+    '''This activation function is just max(fc1,0) (i.e all negative values become 0)
+    This means that (at this stage) negative evidence is thrown out.
+    This has the property of avoiding "vanishing gradients" which kill sections of NNs'''
+    fc1_relu = tf.nn.relu(fc1,'ReLu') #FC Layer 1 (ReLu)
+    '''Dropout has a certain probability of drops a neural connection.
+    Is used while training to prevent over fitting'''
+    keep_prob = tf.placeholder(tf.float32,name = 'Keep_Probability') #Defined during training via feed_dict
+    fc1_drop = tf.nn.dropout(fc1_relu, keep_prob,name='Dropout') #FC Layer 1 (Dropout)
 
-#FC Layer 1 (ReLu)
-'''This activation function is just max(fc1,0) (i.e all negative values become 0)
-This means that (at this stage) negative evidence is thrown out.
-This has the property of avoiding "vanishing gradients" which kill sections of NNs'''
-fc1_relu = tf.nn.relu(fc1)
-
-#FC Layer 1 (Dropout)
-'''Dropout has a certain probability of drops a neural connection.
-Is used while training to prevent over fitting'''
-keep_prob = tf.placeholder(tf.float32) #Defined during training via feed_dict
-fc1_drop = tf.nn.dropout(fc1_relu, keep_prob)
-
-#Fully Connected Layer 2 (turns features into evidence for 10 classes)s
-fc2 = fullycon_layer(fc1_drop, channels[4], channels[5])
+#Fully Connected Layer 2 (turns features into evidence for 10 classes)
+fc2 = fullycon_layer(fc1_drop, channels[4], channels[5],name='FC_Layer_2')
 
 #Output
 '''Softmax activation function applied to get raw probabilities from log-probabilities
 and to normalize probabilities so that results can be easily digested.
 softmax(fc2) = norm(exp(fc2))'''
-y_hat = tf.nn.softmax(fc2)
+y_hat = tf.nn.softmax(fc2,name='Output')
 
 #Define Loss and train step
-y = tf.placeholder(tf.float32, shape=[None, channels[5]]) #nx10 Matrix (One-Hot Vector, Label Data)
-'''fc2 is used for cross_entropy instead of y_hat because the softmax_cross_entropy function does 
-softmax internally to keep the function numerically stable'''
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(fc2, y)) #Cost Function
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy) #Training step (optimizer)
+with tf.name_scope('Training'):
+    with tf.name_scope('Training_Input'):
+        y = tf.placeholder(tf.float32, shape=[None, channels[5]],name='y') #nx10 Matrix (One-Hot Vector, Label Data)
+    '''fc2 is used for cross_entropy instead of y_hat because the softmax_cross_entropy function does 
+    softmax internally to keep the function numerically stable'''
+    with tf.name_scope('Cost_Function'):
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(fc2, y,name='Cost_Function')) #Cost Function
+    with tf.name_scope('Optimizer'):
+        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy,name='Optimizer') #Training step (optimizer)
 
 #Define Accuracy
-correct_prediction = tf.equal(tf.argmax(fc2,1), tf.argmax(y,1)) #List of booleans (correct or not)
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) #Average of above list
+with tf.name_scope('Validation'):
+    with tf.name_scope('Correct_Pred.'):
+        correct_prediction = tf.equal(tf.argmax(fc2,1), tf.argmax(y,1),name='Correct_Pred.') #List of booleans (correct or not)
+    with tf.name_scope('Mean_Accuracy'):
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32),name='Mean_Accuracy') #Average of above list
 
 #Create/Initialize Session
 sess = tf.InteractiveSession() #Create Session
 sess.run(tf.global_variables_initializer()) #Init Variables
 
+#Summery Writer
+writer = tf.summary.FileWriter(save_path + '\logs', sess.graph)
+
 #Training (20,000 batches of 50)
-for i in range(20001):
+for i in range(101):
     batch = mnist.train.next_batch(50)
     if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={x:batch[0], y: batch[1], keep_prob: 1.0})
@@ -138,11 +147,11 @@ for i in range(20001):
 
 #Save Model (the weights and biases
 saver = tf.train.Saver()
-saver.save(sess, save_path)
+saver.save(sess, save_path + '\conv_mnist.chk')
 
 # #Load Model
 # saver = tf.train.Saver()
-# saver.restore(sess, save_path)
+# saver.restore(sess, save_path + '\conv_mnist.chk')
 
 #Accuracy Check (10 batches of 50)
 for i in range(10):
